5a0dacb9bcac6c593f933ac69ddccd0ae50392f0
Oxer11 <17300240035@fudan.edu.cn>
2020-03-28 10:21:31 +0800

    Nearby Page Finished

--- src/models/NearbyData.js
+++ src/models/NearbyData.js
@@ -1,10 +1,5 @@
 import request from '../utils/request';
 
-const randomPosition = () => ({
-  longitude: 120 + Math.random() * 0.01,
-  latitude: 36 + Math.random() * 0.01,
-});
-
 export default {
   namespace: 'nearby',
   state: {},
@@ -13,21 +8,24 @@ export default {
       if (result.data) {
         return result.data;
       }
-
-      return {
-        markers: [
-          { position: randomPosition() },
-          { position: randomPosition() },
-          { position: randomPosition() },
-        ],
-        mapCenter: randomPosition(),
-      };
+      return {};
     },
   },
   effects: {
     *initData(params, sagaEffects) {
       const { call, put } = sagaEffects;
       const url = '/data/nearby';
+      let data = yield call(request, url);
+      yield put({
+        type: 'changeCenter',
+        data: data,
+      });
+    },
+    *search(params, sagaEffects) {
+      const { call, put } = sagaEffects;
+      let lnglat = params.address;
+      const url = "/data/nearby?lng=" + String(lnglat.lng) + "&lat=" + String(lnglat.lat);
+      console.log(url);
       let data = yield call(request, url);
       yield put({
         type: 'changeCenter',
