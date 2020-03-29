151e23b27b4badcfa26dc16b44f156913d42d88e
QiaowenYoung <17307130294@fudan.edu.cn>
2020-03-29 13:34:54 +0800

    domestic map

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,17 +1,27 @@
 import React, { PureComponent } from 'react';
 import { Card, Descriptions, Empty, Row, Col, Tabs, Table } from 'antd';
 import { LineChartOutlined, PieChartOutlined, TableOutlined } from '@ant-design/icons';
+import ProvincesConfirm from '@/components/Charts/ProvincesConfirm';
+import jsonp from 'jsonp'; // 接口jsonp实现跨域
 
 const { Item } = Descriptions;
 const { TabPane } = Tabs;
 const { Meta } = Card;
+const ROOT = 'https://lab.isaaclin.cn/nCoV/api/';
 export default class Welcome extends PureComponent {
   state = {
-    data: {},
+    data: {}, // 国内疫情总览
+    currData: {}, // 国内各省市现存确诊
+    totalData: {}, // 国内各省市累计确诊
   };
 
   componentDidMount = () => {
-    const url = 'https://lab.isaaclin.cn/nCoV/api/overall';
+    this.fetchOverall();
+    this.fetchSinaData();
+  };
+
+  fetchOverall = () => {
+    const url = `${ROOT}overall`;
     fetch(url)
       .then(res => res.json())
       .then(data => {
@@ -19,6 +29,26 @@ export default class Welcome extends PureComponent {
           data: data.results[0],
         });
       });
+  };
+
+  fetchSinaData = () => {
+    const self = this;
+    jsonp('https://interface.sina.cn/news/wap/fymap2020_data.d.json', (err, data) => {
+      const curr = data.data.list.map(item => ({
+        name: item.name,
+        value: item.econNum,
+      }));
+      self.setState({
+        currData: curr,
+      });
+      const total = data.data.list.map(item => ({
+        name: item.name,
+        value: item.value,
+      }));
+      self.setState({
+        totalData: total,
+      });
+    });
   };
 
   renderInfo = () => {
@@ -70,59 +100,57 @@ export default class Welcome extends PureComponent {
     );
   };
 
+  currMap1 = currData => <ProvincesConfirm data={currData} isCurr />;
+
+  sumMap1 = totalData => <ProvincesConfirm data={totalData} isCurr={false} />;
+
   renderMap1 = () => {
-    console.log('中国地图');
+    // 疫情地图
+    const { currData } = this.state;
+    const { totalData } = this.state;
     return (
       <Card>
         <Meta title="疫情地图" avatar={<PieChartOutlined />} />
         <p />
         <Tabs defaultActiveKey="1" onChange={this.callback()}>
           <TabPane tab="现存" key="1">
-            Content of Tab Pane 1
+            {this.currMap1(currData)}
           </TabPane>
           <TabPane tab="累计" key="2">
-            Content of Tab Pane 2
+            {this.sumMap1(totalData)}
           </TabPane>
         </Tabs>
       </Card>
     );
   };
 
-  callback = () => {
-    console.log('tab change');
-  };
+  callback = () => {};
 
-  renderMap2 = () => {
-    console.log('总体曲线');
-    return (
-      <Card>
-        <Meta title="总体曲线" avatar={<LineChartOutlined />} />
-        <p />
-        <Tabs defaultActiveKey="1" onChange={this.callback()}>
-          <TabPane tab="现存" key="1">
-            Content of Tab Pane 1
-          </TabPane>
-          <TabPane tab="新增" key="2">
-            Content of Tab Pane 2
-          </TabPane>
-          <TabPane tab="治愈 / 死亡率" key="3">
-            Content of Tab Pane 2
-          </TabPane>
-        </Tabs>
-      </Card>
-    );
-  };
+  renderMap2 = () => (
+    <Card>
+      <Meta title="总体曲线" avatar={<LineChartOutlined />} />
+      <p />
+      <Tabs defaultActiveKey="1" onChange={this.callback()}>
+        <TabPane tab="现存" key="1">
+          Content of Tab Pane 1
+        </TabPane>
+        <TabPane tab="新增" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="治愈 / 死亡率" key="3">
+          Content of Tab Pane 2
+        </TabPane>
+      </Tabs>
+    </Card>
+  );
 
-  renderTable = () => {
-    console.log('数据列表');
-    return (
-      <Card>
-        <Meta title="数据列表" avatar={<TableOutlined />} />
-        <p />
-        <Table />
-      </Card>
-    );
-  };
+  renderTable = () => (
+    <Card>
+      <Meta title="数据列表" avatar={<TableOutlined />} />
+      <p />
+      <Table />
+    </Card>
+  );
 
   render() {
     return (
